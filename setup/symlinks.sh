9017643a68bfb2ce3e256e30610567b975668502
Marius <marius@freshcolors.ro>
2016-04-29 19:56:58 +0300

    Fix wrong source file path in symlinks creation setup script

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -4,17 +4,18 @@ cd "$(dirname "$BASH_SOURCE")" && source 'utils.sh'
 
 declare -a SYMLINKS=(
     'vim/vimrc'
+    'screen/screenrc'
 )
 
 setup() {
 	local i=''
 	local sourceFile=''
 	local targetFile=''
-
-	for i in ${SYMLINKS[@]}; do
-		sourceFile="$(pwd)/$i"
+	
+    for i in ${SYMLINKS[@]}; do
+		sourceFile="$(cd .. && pwd)/$i"
 		targetFile="$HOME/.$(echo "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
-
+        
 		if [ ! -e "$targetFile" ]; then
 			if $(ln -s $sourceFile $targetFile); then
 				echo_ok "$targetFile -> $sourceFile"
