83f45aafbf0b4a1263d7cc013dd865f804d13646
Marius <marius@freshcolors.ro>
2016-04-30 19:31:46 +0300

    Add support for virtualenvwrapper

---
+++ zsh/config/tools/python/virtualenvwrapper/config.zsh
@@ -0,0 +1,11 @@
+# Only configure if wrapper script exists
+if (( ! $+commands[virtualenvwrapper.sh )); then
+  return 1
+fi
+
+# Set the directory where to place virtual environments
+if [ "$WORKON_HOME" = "" ]; then
+    export WORKON_HOME="$HOME/.virtualenvs"
+fi
+
+source "$commands[virtualenvwrapper.sh]"
